0e41b93fe5a562611c4d5d4ddc74a2c940532d28
Keks <keks@htmlacademy.ru>
2020-08-10 13:07:44 +0300

    :kissing_cat: Добавляет сборку Less на Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,51 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const pug = require('gulp-pug');
-const del = require('del');
-const prettify = require('gulp-prettify');
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
 
+// Styles
 
-function cleanSource(){
-	return del('./source');
+const styles = () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(sync.stream());
 }
 
-function gulpSass(){
-	return gulp.src('./work/sass/main.scss')
-		.pipe(plumber({
-			errorHandler: notify.onError(function(err){
-				return{
-					title: 'Styles',
-					message: err.message
-				}
-			})
-		}))
-		.pipe(sourcemaps.init())
-		.pipe(sass())
-		.pipe(autoprefixer(
-			['last 3 versions'],
-			{cascade: false}
-		))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./source/css'))
-		.pipe(browserSync.stream());
+exports.styles = styles;
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
 }
 
-function gulpPug(){
-	return gulp.src('./work/pug/pages/*.pug')
-		.pipe(plumber({
-			errorHandler: notify.onError(function(err){
-				return{
-					title: 'Pug',
-					message: err.message
-				}
-			})
-		}))
-		.pipe(pug({
-			pretty: true
-		}))
-		.pipe(prettify({
-			indent_size: 2,
-			indent_char: ' ',
-			inline: [],
-			end_with_newline: true
-		}))
-		.pipe(gulp.dest('./source'))
-		.pipe(browserSync.stream());
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", sync.reload);
 }
 
-function copyJS(){
-	return gulp.src('./work/js/**/*.js')
-		.pipe(gulp.dest('./source/js'))
-		.pipe(browserSync.stream());
-}
-function copyLibs(){
-	return gulp.src('work/libs/**/*.*')
-		.pipe(gulp.dest('./source/libs'))
-		.pipe(browserSync.stream());
-}
-function copyImg(){
-	return gulp.src('work/img/**/*.*')
-		.pipe(gulp.dest('./source/img'))
-		.pipe(browserSync.stream());
-}
-
-function copyFonts(){
-	return gulp.src('work/fonts/**/*.*')
-		.pipe(gulp.dest('./source/fonts'))
-		.pipe(browserSync.stream());
-}
-
-function gulpServer(){
-	browserSync.init({
-		server: {baseDir: './source/'}
-	});
-}
-function gulpWatch(){
-	gulp.watch('work/pug/**/*.pug', gulp.series(gulpPug));
-	gulp.watch('work/sass/**/*.scss', gulp.series(gulpSass));
-	gulp.watch('work/js/**/*.js', gulp.series(copyJS));
-	gulp.watch('work/libs/**/*.*', gulp.series(copyLibs));
-	gulp.watch('work/img/**/*.*', gulp.series(copyImg));
-	gulp.watch('work/fonts/**/*.*', gulp.series(copyFonts));
-}
-
-
-gulp.task('default', gulp.series(cleanSource, gulpSass, gulpPug, copyJS, copyLibs, copyImg, copyFonts, gulp.parallel(gulpServer, gulpWatch)));
+exports.default = gulp.series(
+  styles, server, watcher
+);
